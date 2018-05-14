929e9649af3868f8c969d441ad5a1fc3682d37d0
Alexio96 <10495770@polimi.it>
2018-05-14 23:23:49 +0200

    Added some more test class (DiceBag and Model).

---
+++ src/test/java/it/polimi/se2018/TestDiceBag.java
@@ -0,0 +1,47 @@
+package it.polimi.se2018;
+
+import it.polimi.se2018.server.model.DiceBag;
+import it.polimi.se2018.server.model.Die;
+import it.polimi.se2018.server.model.EmptyDiceBagException;
+import org.junit.Test;
+
+import static it.polimi.se2018.server.model.DiceBag.*;
+import static org.junit.Assert.*;
+
+import static org.junit.Assert.fail;
+
+public class TestDiceBag {
+    @Test
+    public void testBag() throws EmptyDiceBagException {
+        int n1=0,n2=0,n3=0,n4=0,n5=0;
+        Die val = null;
+        DiceBag diceBag = DiceBag.instance();
+
+        for(int i=0; i<90; i++) {
+            val = diceBag.extract();
+
+            switch(val.getColor()){
+                case YELLOW:
+                    n1++;
+                    break;
+                case GREEN:
+                    n2++;
+                    break;
+                case RED:
+                    n3++;
+                    break;
+                case BLUE:
+                    n4++;
+                    break;
+                case PURPLE:
+                    n5++;
+                    break;
+            }
+        }
+
+        if(!(n1 == n2 && n2 == n3 && n3 == n4 && n4 == n5 && n5 == 18)){
+            fail();
+        }
+
+    }
+}
---
+++ src/test/java/it/polimi/se2018/TestModel.java
@@ -0,0 +1,72 @@
+package it.polimi.se2018;
+
+import it.polimi.se2018.server.model.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestModel {
+    @Test
+    public void TestAddingPlayers() throws MaxNumPlayersException {
+        //tests the maximum number of players is respected.
+        Model ModelExample = Model.instance();
+        Player p1 = new Player("Alessio");
+        Player p2 = new Player("Sfidante");
+        Player p3 = new Player("Tizio");
+        Player p4 = new Player("Caio");
+        Player p5 = new Player("Sempronio");
+
+        try {
+            ModelExample.addPlayer(p1);
+            ModelExample.addPlayer(p2);
+            ModelExample.addPlayer(p3);
+            ModelExample.addPlayer(p4);
+            ModelExample.addPlayer(p5);
+            fail();
+        }catch (MaxNumPlayersException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void TestUpdatingTurn() throws InvalidTurnException, MaxNumPlayersException {
+        //tests if the end of the match is respected and if the order of some casual turn are respected too.
+        Model modelExample = Model.instance();
+        Player p1 = new Player("Alessio");
+        Player p2 = new Player("Sfidante");
+        Player p3 = new Player("Tizio");
+        modelExample.addPlayer(p1);
+        modelExample.addPlayer(p2);
+        modelExample.addPlayer(p3);
+
+        for(int i=1; i<=59; i++){
+            System.out.printf("Turn:%2d   Round:",modelExample.getTurn());
+            System.out.println(modelExample.getRound());
+
+            if(i==3){
+                if(!(modelExample.getTurn()==3)){
+                    fail();
+                }
+            }
+            if(i==4){
+                if(!(modelExample.getTurn()==3)){
+                    fail();
+                }
+            }
+            if(i==6){
+                if(!(modelExample.getTurn()==1)){
+                    fail();
+                }
+            }
+            modelExample.updateTurn();
+        }
+
+        try{
+            modelExample.updateTurn();
+            fail();
+        }catch (InvalidTurnException e){
+            e.printStackTrace();
+        }
+
+    }
+}
